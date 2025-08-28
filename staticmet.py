bee5b9f5f7e21d4b250a6f9c5a4b867caf7955ef
DHAVASIDHARAN S <dhavasidharan0907@gmail.com>
2025-08-28 12:51:07 +0530

    Add files via upload

---
+++ staticmet.py
@@ -0,0 +1,9 @@
+class College:
+    cname="PANIMALR ENGINEERING COLLEGE"
+    loc="CHENNAI"
+    @staticmethod
+    def show():
+        print("Welcomr to Panimalar")
+c=College()
+College.show()
+c.show()
