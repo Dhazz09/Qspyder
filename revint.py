7216e4b3e83298e740d660b65370862bfa7fe96c
DHAVASIDHARAN S <dhavasidharan0907@gmail.com>
2025-08-30 20:44:29 +0530

    Update revint.py

--- revint.py
+++ revint.py
@@ -1,8 +1,9 @@
-# 🔁 Number Reverser using While Loop
+#  Number Reverser using While Loop
 num = int(input("Enter a number to reverse: "))
 rev = 0
 while num > 0:
     digit = num % 10         
     rev = rev * 10 + digit   
     num = num // 10          
-print(f"🔄 Reversed number: {rev}")
+
+print(f" Reversed number: {rev}")
