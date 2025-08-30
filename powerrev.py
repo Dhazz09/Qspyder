4e9ab3e76d1708baefcdf025f898ad2d6ccdfc26
DHAVASIDHARAN S <dhavasidharan0907@gmail.com>
2025-08-30 20:43:02 +0530

    Update powerrev.py

--- powerrev.py
+++ powerrev.py
@@ -1,4 +1,4 @@
-# 🔄 Word Reverser Dictionary Builder using for loop
+# Word Reverser Dictionary Builder using for loop
 
 
 S = 'power star'
@@ -9,4 +9,5 @@ for word in words:
     for i in range(len(word) - 1, -1, -1):
         reversed_word += word[i]
     reversed_dict[word] = reversed_word
+
 print(f"Output: {reversed_dict}")
